95ca11c24cde61be204f00e4c442385e15f7767a
Eric Lam <elam3@ualberta.ca>
2018-11-17 18:29:40 -0700

    - Added in node recognition of its neighbours. - Added a basic ship prefab as a test. - Added foundation for player controls.

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -1,22 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(GameGridGenerator))]
 public class GameGrid : MonoBehaviour {
     
     public LayerMask untraversableMask;
-    private Node[,] grid;
+
+    public static Node selectedNode;
+
+    private static Node[,] grid;
 
     private void Awake()
     {
-        
+        CalculateGrid();
     }
 
-    public void GenerateGrid()
+    public void CalculateGrid()
     {
-        Transform grid = transform.GetChild(0);
+        Transform generatedGrid = transform.GetChild(0);
+        GameGridGenerator generator = GetComponent<GameGridGenerator>();
 
+        int rowSize = (int)generator.gridWorldSize.x;
+        int colSize = (int)generator.gridWorldSize.y;
 
+        grid = new Node[colSize, rowSize];
+
+        if (generatedGrid != null && (rowSize * colSize == generatedGrid.childCount))
+        {
+            int index = 0;
+
+            for (int y = 0; y < colSize; y++)
+            {
+                for (int x = 0; x < rowSize; x++)
+                {
+                    Node node = generatedGrid.GetChild(index).GetComponent<Node>();
+                    grid[y, x] = node;
+                    node.gridPosition = new int[]{y, x};
+                    index++;
+                }
+            }
+        }
+    }
+
+    // TODO: Use Range parameter.
+    public static Node[] GetNeighbours(int[] gridPosition, int range)
+    {
+        int row = gridPosition[0];
+        int col = gridPosition[1];
+
+        List<Node> nodeList = new List<Node>();
+
+        // Left Neighbour.
+        if(row > 0)
+        {
+            nodeList.Add(grid[row - 1, col]);
+        }
+
+        // Right Neighbour.
+        if(row < grid.GetLength(0) - 1)
+        {
+            nodeList.Add(grid[row + 1, col]);
+        }
+
+        // Top Neighbour.
+        if(col > 0)
+        {
+            nodeList.Add(grid[row, col - 1]);
+        }
+
+        // Bottom Neighbour.
+        if(col < grid.GetLength(1) - 1)
+        {
+            nodeList.Add(grid[row, col + 1]);
+        }
+
+        return nodeList.ToArray();
     }
 
 
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -5,16 +5,52 @@ using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour, IPointerClickHandler {
 
+    public Transform unit;  // Current occupant.
     public bool traversable = true;
     public Vector3 worldPosition;
+    public int[] gridPosition;
+
+    private SpriteRenderer spriteRen;
+    private Color32 originalColour;
 
     private void Awake()
     {
         worldPosition = transform.position;
+
+        spriteRen = GetComponent<SpriteRenderer>();
+        originalColour = spriteRen.color;
     }
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        Debug.Log(name + " Game Object Clicked!");
+        // TODO: Get unit's Range.
+        if(GameGrid.selectedNode != null)
+        {
+            GameGrid.selectedNode.ResetNeighbours();   
+        }
+
+        GameGrid.selectedNode = this;
+        // Hard-coded value is range value.
+        Node[] neighbours = GameGrid.GetNeighbours(gridPosition, 1);
+
+        for (int i = 0; i < neighbours.Length; i++)
+        {
+            neighbours[i].SetColour(new Color32(0, 255, 0, 255));
+        }
+    }
+
+    public void ResetNeighbours()
+    {
+        Node[] neighbours = GameGrid.GetNeighbours(gridPosition, 1);
+        for (int i = 0; i < neighbours.Length; i++)
+        {
+            if(neighbours[i].traversable)
+            neighbours[i].SetColour(originalColour);
+        }
+    }
+
+    private void SetColour(Color32 newColour)
+    {
+        spriteRen.color = newColour;
     }
 }
