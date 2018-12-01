a3d4b30f619ad6cf6c0590960799946d7c1c90b0
Christopher Dubeau <Christopherdubeau@gmail.com>
2018-12-01 13:49:16 -0700

    Added Controller to Bridge

---
+++ Assets/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SceneController : MonoBehaviour {
+	public Vector3 target;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, target, 1 * Time.deltaTime);
+	}
+}
