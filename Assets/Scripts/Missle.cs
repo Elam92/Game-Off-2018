0650812bc318b8e77b5e782a5ca65b78f3cc1518
Christopher Dubeau <christopherdubeau@gmail.com>
2018-11-30 19:10:38 -0700

    Added Turn Recognition
    -Added turn controls
    -Added system to stop players on Ai turn
    -Added checks to stop targeting allies
    -Changed Ship owner marker from a bool in Ship.cs to a tag based system
    -Began implementing a visable system to indicate shooting

---
+++ Assets/Scripts/Missle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missle : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        transform.position = GetComponentInParent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void update () {
+        if (target != null)
+        {
+            Debug.Log("ilive");
+            //while (transform.position != target.transform.position)
+            //{
+                //transform.Translate(Vector3.MoveTowards(transform.position, target.transform.position, .1f));
+            //}
+            Destroy(this);
+        }
+	}
+}
