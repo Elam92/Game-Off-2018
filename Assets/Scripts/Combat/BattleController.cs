eecbddb8c44c5a6e8b43966b9ac636826000d1c1
Eric <elam3@ualberta.ca>
2018-12-13 11:39:27 -0700

    Starting new implementation of how combat is done under the hood.

---
+++ Assets/Scripts/Combat/BattleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour {
+
+    private State currentState;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentState.Tick();
+    }
+
+    public void SetState(State state)
+    {
+        if (currentState != null)
+            currentState.OnStateExit();
+
+        currentState = state;
+        gameObject.name = "Cube - " + state.GetType().Name;
+
+        if (currentState != null)
+            currentState.OnStateEnter();
+    }
+}
